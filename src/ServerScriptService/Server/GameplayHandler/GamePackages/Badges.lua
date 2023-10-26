b2e9a029fa7de44764688781431f346e821c4e26
DevTreason <59517946+DevTreason@users.noreply.github.com>
2023-10-25 21:22:36 -0500

    Create Badges.lua

---
+++ src/ServerScriptService/Server/GameplayHandler/GamePackages/Badges.lua
@@ -0,0 +1,27 @@
+local module = {}
+
+module.Enabled = true
+
+--/ Services /--
+local ServerStorage = game:GetService("ServerStorage")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+--/ Modules /--
+local Modules = {}
+for _, module in pairs(script:GetChildren()) do
+    Modules[module.Name] = require(module)
+end
+
+--/ Private Variables /--
+
+
+--/ Private Functions /--
+
+
+--/ Public Functions /--
+function module:Start()
+    
+end
+
+return module
