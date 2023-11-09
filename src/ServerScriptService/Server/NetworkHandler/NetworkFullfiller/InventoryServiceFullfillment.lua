e41a1fb731b01f30593efebf7c3ffdb39811d613
DevTreason <59517946+DevTreason@users.noreply.github.com>
2023-11-08 18:58:36 -0600

    Create InventoryServiceFullfillment.lua

---
+++ src/ServerScriptService/Server/NetworkHandler/NetworkFullfiller/InventoryServiceFullfillment.lua
@@ -0,0 +1,26 @@
+local module = {}
+
+--/ Services /--
+local ServerScriptService = game:GetService("ServerScriptService")
+local ServerStorage = game:GetService("ServerStorage")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+--/ Modules /--
+local Modules = {}
+for _, module in pairs(script:GetChildren()) do
+    Modules[module.Name] = require(module)
+end
+
+--/ Private Variables /--
+
+
+--/ Private Functions /--
+
+
+--/ Public Functions /--
+function module:Start()
+    
+end
+
+return module 
