31bf49c0855864693c8be1cb4391e4fcf6c030ab
DevTreason <59517946+DevTreason@users.noreply.github.com>
2023-11-26 20:20:37 -0600

    Update .source.lua

--- src/ServerScriptService/Server/DataHandler/.source.lua
+++ src/ServerScriptService/Server/DataHandler/.source.lua
@@ -2,83 +2,29 @@ local module = {}
 
 --/ Services /--
 local ServerStorage = game:GetService("ServerStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
+--/ Variables /--
+local Server = ServerScriptService.Server
+
 --/ Modules /--
-local Modules = {}
-for _, module in pairs(script:GetChildren()) do
-    Modules[module.Name] = require(module)
-end
+local Modules = {
+    ["HandlerCore"] = {
+        ["DataService"] = require(Server.DataHandler.DataService),
+    }
+}
 
 --/ Private Variables /--
-local Prefix = "DEVELOPMENT_01"
-local ProfileTemplate = Modules.DefaultData
-local ProfileStore = Modules.ProfileService.GetProfileStore("PlayerData", ProfileTemplate)
-local Profiles = {}
+
 
 --/ Private Functions /--
-local function PlayerAdded(player)
-    local Profile = ProfileStore:LoadProfileAsync(Prefix..player.UserId)
-    if Profile ~= nil then
-        Profile:AddUserId(player.UserId)
-        Profile:Reconcile()
-        Profile:ListenToRelease(function()
-            Profiles[player] = nil
-            player:Kick("The profile might have been loaded on another server.")
-        end)
-        if player:IsDescendantOf(Players) then
-            Profiles[player] = Profile
-        else
-            Profile:Release()
-        end
-    else
-        player:Kick("Roblox couldn't load your data. Please rejoin the game.")
-    end
-end
 
-local function PlayerRemoving(player)
-    local Profile = Profiles[player]
-    if Profile then
-        Profile:Release()
-    end
-end
 
 --/ Public Functions /--
-function module.GetProfile(player)
-    while not Profiles[player] do task.wait() end
-    return Profiles[player]
-end
-
-function module.GetDataOfSpecificType(player, dataType)
-    repeat task.wait() until Profiles[player]
-    local Profile = Profiles[player]
-    if Profile then
-        return Profile.Data[dataType]
-    end
-end
-
-function module.UpdateDataOfSpecificType(player, dataType, data)
-    local Profile = Profiles[player]
-    local Name = data[1]
-    local Type = data[2]
-    local Amount = data[3]
-    if Profile then
-        if typeof(Amount) == "number" and not Type and not Name then
-            Profile.Data[dataType] += Amount
-        elseif typeof(Amount) == "number" and Type then
-            Profile.Data[dataType][Type] += Amount
-        elseif typeof(Amount) == "boolean" then
-            Profile.Data[dataType][Name][Type] = Amount
-        end
-    end
-end
-
 function module:Start()
-    Players.PlayerAdded:Connect(PlayerAdded)
-    Players.PlayerRemoving:Connect(PlayerRemoving)
+    Modules.HandlerCore.DataService:Start()
 end
-
-
 
 return module
