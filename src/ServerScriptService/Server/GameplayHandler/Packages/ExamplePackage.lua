27bcb883d1ed2e4685ca133055b7b94a66e9897c
DevTreason <59517946+DevTreason@users.noreply.github.com>
2023-12-08 11:30:46 -0600

    Update ExamplePackage.lua

--- src/ServerScriptService/Server/GameplayHandler/Packages/ExamplePackage.lua
+++ src/ServerScriptService/Server/GameplayHandler/Packages/ExamplePackage.lua
@@ -10,7 +10,9 @@ local Modules = {
     ["Utilities"] = {
         ["TimerUtility"] = require(ReplicatedStorage.FoundationFramework.Utilities.TimerUtility),
         ["TweenUtility"] = require(ReplicatedStorage.FoundationFramework.Utilities.TweenUtility),
-        ["HTTPUtility"] = require(ReplicatedStorage.FoundationFramework.Utilities.HTTPUtility)
+        ["HTTPUtility"] = require(ReplicatedStorage.FoundationFramework.Utilities.HTTPUtility),
+        ["CharacterUtility"] = require(ReplicatedStorage.FoundationFramework.Utilities.CharacterUtility),
+        ["LogUtility"] = require(ReplicatedStorage.FoundationFramework.Utilities.LogUtility),
     }
 }
 
@@ -21,31 +23,6 @@ local Modules = {
 
 
 --/ Public Functions /--
-local Timer = Modules.Utilities.TimerUtility.new(5)
-Timer:Connect(function()
-    print("Timer has been connected!")
-end)
-Timer:Start()
-Timer:OnTick(function()
-    print("Timer is ticking!", Timer.time)
-end)
-Timer:AndThen(function()
-    print("Timer has ended!")
-end)
-
-Players.PlayerAdded:Connect(function(player)
-    local PlayerGui = player:WaitForChild("PlayerGui")
-    local Frame = PlayerGui:WaitForChild("MainUI"):WaitForChild("Frame")
-    local Tween = Modules.Utilities.TweenUtility.new(Frame, TweenInfo.new(5), {Position = UDim2.new(0.5, 0, 0.5, 0)})
-    Tween:Play()
-    Tween:Completed(function()
-        print("Tween has completed!")
-    end)
-end)
-
-local GUID = Modules.Utilities.HTTPUtility.new():GenerateGUID()
-print(GUID)
-
 
 
 return module
